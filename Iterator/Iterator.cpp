2beebbe38d2b83f53d9043ede711ef01b50eeeab
Traums <42118465+Traums@users.noreply.github.com>
2022-06-14 18:11:34 +0500

    PreBuild

---
+++ Iterator/Iterator.cpp
@@ -0,0 +1,41 @@
+//---------------------------------------------------------------------------
+
+#pragma hdrstop
+
+#include "Iterator.h"
+#include "FileSystem.h"
+#include "Main.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+Iterator::Iterator(AbstractFS* FS) :ClusterNumber(0), ClustersTotal(FS->GetTotalClusters()), Done(false), FS(FS)
+{
+	byte* dataBuffer = new BYTE[FS->GetBytesPerCluster()];
+}
+void Iterator::First(){
+	ClusterNumber = 1;
+}
+void Iterator::Last(){
+	ClusterNumber = ClustersTotal;
+}
+void Iterator::Next(){
+	ClusterNumber++;
+}
+int Iterator::Current(){
+	return ClusterNumber;
+}
+bool Iterator::IsDone(){
+	return (ClusterNumber < ClustersTotal && Done == false);
+}
+unsigned long Iterator::GetClusterNumber(){
+	return ClusterNumber;
+}
+void Iterator::Stop(){
+	Done = true;
+}
+byte* Iterator::GetDataOnCluster(){
+	dataBuffer = FS->ReadCluster(ClusterNumber, dataBuffer);
+	BYTE* ptrData = dataBuffer;
+	return ptrData;
+}
+
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Lab_last_remade
  
+
+https://dev64.wordpress.com/2012/03/26/fat32-read-in-c/
+
+https://studfile.net/preview/478497/page:24/
